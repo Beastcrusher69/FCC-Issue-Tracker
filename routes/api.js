181dff7df04e97606122bb9728ddf7726dae3c89
Jay <jaykapadia389@gmail.com>
2023-04-21 23:49:52 +0530

    completed get and post req

--- routes/api.js
+++ routes/api.js
@@ -10,7 +10,8 @@ const IssueSchema =  mongoose.Schema(
     created_by : {type : String , required : true},
     assigned_to : String ,
     open : Boolean ,
-    status_text : String
+    status_text : String,
+    project : String
   }
 )
 
@@ -23,18 +24,61 @@ const ProjectSchema = mongoose.Schema({
 
 const Project = mongoose.model('Project', ProjectSchema )
 
-
 module.exports = function (app) {
 
   app.route('/api/issues/:project')
   
-    .get(function (req, res){
-      let project = req.params.project;
+    .get( async function (req, res){
+
+
+       let project = req.params.project;
+
+      const {
+        _id,
+        open,
+        issue_title,
+        issue_text,
+        created_by,
+        assigned_to,
+        status_text
+      } = req.query ;
+
+      const match = await Project.aggregate([
+        
+        { $match : { name : project}},
+        {$unwind : "$issues" },
+                        _id != undefined 
+                         ?  { $match : { "$issues._id" : ObjectId(_id)}}
+                        :{ $match : {}},
+                        open != undefined 
+                         ?  { $match : { "$issues.open" : open}}
+                        :{ $match : {}},
+                        issue_title != undefined 
+                         ?  { $match : { "$issues.issue_title" : issue_title}}
+                        :{ $match : {}},
+                        issue_text != undefined 
+                         ?  { $match : { "$issues.issue_text" : issue_text}}
+                        :{ $match : {}},
+                        created_by != undefined 
+                         ?  { $match : { "$issues.created_by" : created_by}}
+                        :{ $match : {}},
+                        assigned_to != undefined 
+                         ?  { $match : { "$issues.assigned_to" : assigned_to}}
+                        :{ $match : {}},
+                        status_text != undefined 
+                         ?  { $match : { "$issues.status_text" : status_text}}
+                        :{ $match : {}}
+                        ])
+
+        
+        let mappedData = match.map((item)=> item.issues)
+        res.json(mappedData)
       
-      
+                      
+    
     })
     
-    .post(function (req, res){
+    .post(async function (req, res){
       let project = req.params.project;
 
       let { issue_title
@@ -42,6 +86,10 @@ module.exports = function (app) {
            ,created_by
            ,assigned_to
            ,status_text} = req.body ; 
+
+      if(!issue_title || !issue_text || !created_by ){
+        res.json({ error: 'required field(s) missing' })
+      }
 
       let newIssue = new Issue({
         issue_title : issue_title || "",
@@ -51,39 +99,28 @@ module.exports = function (app) {
         created_by : created_by || "",
         assigned_to : assigned_to || "" ,
         open : true ,
-        status_text : status_text || ""
+        status_text : status_text || "" ,
+        project 
       })
 
-    
-      Project.findOne({name : project}).then(err ,projectData){
+      Issue.create(newIssue);
+      
+      const projectData = await Project.findOne({name : project});
 
-        if(err){
-          res.send({err : "error"})
+    
+        if(!projectData){
+          const projectData = new Project({name : project})
+          projectData.issues.push(newIssue);
+          projectData.save();
+          res.json(newIssue);
         }
         else{
-        res.send(projectData);
+          projectData.issues.push(newIssue);
+          projectData.save();
+          res.json(newIssue);
           
-        }
-        
-      //   if(!projectData){
-      //     const projectData = new Project({
-      //       name: project,
-      //       issues : []
-      //     })
-
-      //     projectData.issues.push(newIssue);
-      //     projectData.save();
-      //     res.json(newIssue);
-
-         
-      //   }
-      //   else{
-      //     projectData.issues.push(newIssue);
-      //     projectData.save();
-      //     res.json(newIssue);
-            
-      // }
       }
+      
   
     })
     
@@ -96,5 +133,7 @@ module.exports = function (app) {
       let project = req.params.project;
       
     });
+
+  
     
 };
